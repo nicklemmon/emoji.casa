fcd554546bf5c3f08c2a7d8ffc65a58437a46d03
Nick Lemmon <nick.lemmon@gmail.com>
2023-01-08 14:41:06 -0500

    Removes unncessary Vite config options

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // const env = loadEnv(mode, process.cwd(), '')
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
   }
