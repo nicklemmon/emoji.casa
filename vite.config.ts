8a1eb449d1c68313225fd5586b00de950c5261e2
Nick Lemmon <nick.lemmon@gmail.com>
2024-03-08 13:39:36 -0500

    Adds additional compression for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
+import compression from 'vite-plugin-compression'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+  switch (mode) {
+    case 'development': {
+      return {
+        plugins: [react()],
+      }
+    }
+
+    case 'production': {
+      return {
+        plugins: [react(), compression()],
+      }
+    }
   }
 })
